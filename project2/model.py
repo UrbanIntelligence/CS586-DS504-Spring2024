f9776ec714548aefcf143dd43ad7836d1d1cc676
Mingzhi Hu <115661115+mhu3@users.noreply.github.com>
2024-02-03 21:08:15 -0500

    Add files via upload
    model structure

---
+++ project2/model.py
@@ -0,0 +1,27 @@
+# model.py
+
+import torch.nn as nn
+
+class TaxiDriverClassifier(nn.Module):
+    """
+    Neural network model for classifying taxi drivers.
+    """
+    def __init__(self, input_dim, output_dim):
+        super(TaxiDriverClassifier, self).__init__()
+
+        ###########################
+        # YOUR IMPLEMENTATION HERE #
+        ###########################
+
+    def forward(self, x):
+        """
+        In the forward function we accept a Tensor of input data and we must return
+        a Tensor of output data. We can use Modules defined in the constructor as
+        well as arbitrary operators on Tensors.
+        """
+        ###########################
+        # YOUR IMPLEMENTATION HERE #
+
+        ###########################
+        return x
+    
